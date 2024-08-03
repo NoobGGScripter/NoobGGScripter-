12ee01d0eee15fd9af70e99af47d7f20ef33c49a
NoobGGScripter <odalipeace@yahoo.com>
2024-08-03 13:15:54 +0100

    Add files via upload

---
+++ cal.py
@@ -0,0 +1,102 @@
+import tkinter as tk
+import colorama
+colorama.init()
+calculation = ""
+
+def add_to_calculation(symbol):
+	global calculation
+	calculation += str(symbol)
+	text_result.delete(1.0,  "end")
+	text_result.insert(1.0, calculation)
+	
+	
+def evaluate_calculation():
+	global calculation
+	try:
+		calculation = str(eval(calculation))
+		text_result.delete(1.0, "end")
+		text_result.insert(1.0, calculation)
+	except:
+			clear_field()
+			text_result.insert(1.0, "Error")
+					
+				
+def clear_field():
+	global calculation
+	calculation = ""
+	text_result.delete(1.0, "end")
+
+	
+	
+root = tk.Tk()
+root.geometry("50x20")
+
+
+
+text_result = tk.Text(root, height=5, width=16, font=("Arial", 24))
+text_result.grid(columnspan=20)
+
+btn_1 = tk.Button(root, text="1", command=lambda: add_to_calculation(1),width=2, font=("Arial", 26))
+btn_1.grid(row=2, column=1)
+
+btn_2 = tk.Button(root, text="2", command=lambda: add_to_calculation(2),width=2, font=("Arial", 26))
+btn_2.grid(row=2, column=2)
+
+btn_3 = tk.Button(root, text="3", command=lambda: add_to_calculation(3),width=2, font=("Arial", 26))
+btn_3.grid(row=2, column=3)
+
+btn_4 = tk.Button(root, text="4", command=lambda: add_to_calculation(4),width=2, font=("Arial", 26))
+btn_4.grid(row=3, column=1)
+
+btn_5 = tk.Button(root, text="5", command=lambda: add_to_calculation(5),width=2, font=("Arial", 27))
+btn_5.grid(row=3, column=2)
+
+btn_6 = tk.Button(root, text="6", command=lambda: add_to_calculation(6),width=2, font=("Arial", 26))
+btn_6.grid(row=3, column=3)
+
+btn_7 = tk.Button(root, text="7", command=lambda: add_to_calculation(7),width=2, font=("Arial", 26))
+btn_7.grid(row=4, column=1)
+
+btn_8 = tk.Button(root, text="8", command=lambda: add_to_calculation(8),width=2, font=("Arial", 26))
+btn_8.grid(row=4, column=2)
+
+btn_9 = tk.Button(root, text="9", command=lambda: add_to_calculation(9),width=2, font=("Arial", 26))
+btn_9.grid(row=4, column=3)
+
+btn_0 = tk.Button(root, text="0", command=lambda: add_to_calculation(0),width=2, font=("Arial", 26))
+btn_0.grid(row=5, column=2)
+
+btn_plus = tk.Button(root, text="+", command=lambda: add_to_calculation("+"),width=2, font=("Arial", 26))
+btn_plus.grid(row=2, column=4)
+
+btn_minus = tk.Button(root, text="-", command=lambda: add_to_calculation("-"),width=2, font=("Arial", 26))
+btn_minus.grid(row=3, column=4)
+
+btn_mul = tk.Button(root, text="*", command=lambda: add_to_calculation("*"), width=2, font=("Arial", 26))
+btn_mul.grid(row=4, column=4)
+
+btn_div = tk.Button(root, text="/", command=lambda: add_to_calculation("/"), width=2, font=("Arial", 26))
+btn_div.grid(row=5, column=4)
+
+btn_op = tk.Button(root, text="(", command=lambda: add_to_calculation("("),width=2, font=("Arial", 26))
+btn_op.grid(row=5, column=1)
+
+btn_cl = tk.Button(root, text=")", command=lambda: add_to_calculation(")"),width=2, font=("Arial", 26))
+btn_cl.grid(row=5, column=3)
+
+btn_c = tk.Button(root, text="C", command=clear_field,width=2, font=("Arial", 26))
+btn_c.grid(row=6, column=1)
+
+btn_eq = tk.Button(root, text="=", command=evaluate_calculation, width=2, font=("Arial", 26))
+btn_eq.grid(row=6, column=4)
+
+btn_mull = tk.Button(root, text="//", command=lambda: add_to_calculation("//"),width=2, font=("Arial", 26))
+btn_mull.grid(row=6, column=2)
+
+btn_po = tk.Button(root, text=".", command=lambda: add_to_calculation("."),width=2, font=("Arial", 26))
+btn_po.grid(row=6, column=3)
+root.mainloop()	
+
+	
+	
+	
